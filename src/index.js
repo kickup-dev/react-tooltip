567917662f2127a75c3e08859c4180820bc37974
Jimmy Harrison <jtharrison91@gmail.com>
2021-02-25 10:14:43 -0500

    fix: do not delay show if tooltip is already shown

--- src/index.js
+++ src/index.js
@@ -533,7 +533,6 @@ class ReactTooltip extends React.Component {
     const { delayShow, disable } = this.state;
     const { afterShow } = this.props;
     const placeholder = this.getTooltipContent();
-    const delayTime = parseInt(delayShow, 10);
     const eventTarget = e.currentTarget || e.target;
 
     // Check if the mouse is actually over the tooltip, if so don't hide the tooltip
@@ -545,6 +544,8 @@ class ReactTooltip extends React.Component {
     if (this.isEmptyTip(placeholder) || disable) {
       return;
     }
+
+    const delayTime = !this.state.show ? parseInt(delayShow, 10) : 0;
 
     const updateState = () => {
       if (
@@ -569,7 +570,7 @@ class ReactTooltip extends React.Component {
     };
 
     clearTimeout(this.delayShowLoop);
-    if (delayShow) {
+    if (delayTime) {
       this.delayShowLoop = setTimeout(updateState, delayTime);
     } else {
       updateState();
